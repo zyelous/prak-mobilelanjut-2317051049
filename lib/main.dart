530883b48eb65c23febf8ec982069ebc62009ccf
zahracitraaprilliana <zahracitraaprilliana@gmail.com>
2025-09-02 08:10:05 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+        title: const Text('PRAKTIKUM MOBILE LANJUT'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hallo Nama saya Zahra Citra Apriliana',
+          textAlign: TextAlign.center,
+          // style: TextStyle(fontSize: 16),
+        )
+      ) 
+      ) 
+    );
+  }
+}
